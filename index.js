134d906c7502e0e7a4b451409391011c4ba53d7b
Henry Aronofsky <henryaronofsky@Henrys-MBP.lan>
2022-11-28 22:55:48 -0500

    sup

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-var path = require('path');
-const request = require('request');
-var express = require('express');
-var app = express();
-var fs = require("fs");
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+app.use(bodyParser.json({limit: myLimit}));
 
-app.get('/', function (req, res) {
-  request('https://api.opensea.io/user/0x4f0e1AfCEfd307ef0f306959c970Fe96839349Bc?format=json', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log( body );
-      res.end( body );
+app.all('/:account', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        request({
+                url: `https://api.opensea.io/user/${req.params.account}?format=json`, 
+                headers: {'Authorization': req.header('Authorization')}
+            },
+            function (error, response, body) {
+                if (error) {
+                    console.error(error)
+                }
+            }).pipe(res);
     }
-  });
-})
+});
 
-app.listen(8081)
+app.listen(3000);
