202f474c1d18d5ea6d9c51c6f172f6a43eeabdb3
Henry Aronofsky <henryaronofsky@Henrys-MacBook-Pro.local>
2022-12-02 16:28:20 -0500

    asdf

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const router = express.Router();
 var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
 app.use(bodyParser.json({limit: myLimit}));
 
-router.get('/:account', function (req, res, next) {
+app.all('/:account', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
