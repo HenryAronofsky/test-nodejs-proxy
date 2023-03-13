de554794705b7f7053d18a5fe095b63aa914c4a0
Henry <63755578+HenryAronofsky@users.noreply.github.com>
2023-03-13 19:43:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var serverData = {
     "planetData": [],
     "barData": [],
 };
-app.all('/:account', function (req, res, next) {
+app.all('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
